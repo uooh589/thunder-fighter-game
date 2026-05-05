fc5b06fff5fa823bf78a0ae9c7ef42a04e86282d
uooh589 <uooh589@users.noreply.github.com>
2026-05-05 14:04:00 +0800

    chore: PWA support, Windows bat launcher, icon

---
+++ 启动游戏.bat
@@ -0,0 +1,10 @@
+@echo off
+title 雷霆战机 - Thunder Fighter
+echo ⚡ 雷霆战机 - Thunder Fighter
+echo.
+echo 正在启动游戏...
+echo 如果浏览器未打开，请手动双击 index.html
+echo.
+start "" "index.html"
+timeout /t 3 /nobreak >nul
+exit
